4bf975d04a94d6401a2a8b3949110f5fdae14ca5
guru1121 <gurunandm@gmail.com>
2022-11-15 14:40:57 +0530

    index

---
+++ lec5/index.js
@@ -0,0 +1,73 @@
+// function addTwoStrings(x,y){
+//     console.log('hii my name is' + x+y)
+// }
+
+// addTwoStrings("abhi","newton")
+
+//var calculateString = function(x){
+//    x()
+//    console.log('hii my name is')
+//}
+//function name(){
+//    console.log("guru")
+//}
+
+//calculateString(name)
+
+// let student = {
+//     name:
+//     age:
+// }
+// let x = new Object()
+// console.log(x)
+
+//or
+
+// let x1 = {}
+// console.log(x1)
+// let student = {
+//     name: "avi",
+//     age: 21,
+//     getName(){
+//         console.log("avi")
+//     },
+//     physicalFeatures: {
+//            haircolor: "black"
+//     }
+// }
+
+
+// console.log(student.name)
+// console.log(student.physicalFeatures.haircolor)
+// console.log(student.age)
+// student.getName()
+
+// function reverse(s){
+// console.log(s.split("").reverse().join(" "))
+// }
+// reverse("tauseef")
+
+// function checkAnagrams(s1, s2){
+// if(s1.split("").sort().join("") === s2.split("").sort().join("")){
+// console.log("Anagram")
+// }else{
+//     console.log( "not a anagram")
+// }
+// }
+// checkAnagrams("act","tac")
+// checkAnagrams("actor ","tractor")
+// checkAnagrams("qwerty","dig")
+
+// let x =[ 1, 2, "12121", 1]
+ 
+// console.log(typeof(x));
+
+// var y = "abcd";
+// console.log(typeof(y));
+
+// var z = 1;
+// console.log(typeof(z));
+
+// var u = 0
+// var res =u !=1;
+// console.log(typeof(res))
